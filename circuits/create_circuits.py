8f69abdcc119affcdf2ff2c885f485a088ff9924
Jacob Groner <jacobgroner@gmail.com>
2024-01-18 15:57:25 -0800

    fixed naming conflicts

--- circuits/create_circuits.py
+++ circuits/create_circuits.py
@@ -7,13 +7,13 @@ restricted = ['%', '-', '^', '@', '#', '$']
 def create_move_circuit(alphabet):
     with open('move_circuit_dynamic.txt', 'w') as f:
         for c in alphabet:
-            f.write('move1 ' + c + ' @ r m' + c + '\n')
+            f.write('move1 ' + c + ' @ r m_' + c + '\n')
         f.write('move1 % _ r move2\n')
         for c in alphabet:
-            f.write('\nm' + c + ' $ * r m' + c + '1\n')
-            f.write('m' + c + ' * * r *\n\n')
-            f.write('m' + c + '1 _ '+ c +' l move \n')
-            f.write('m' + c + '1 * * r *\n')
+            f.write('\nm_' + c + ' $ * r m_' + c + '1\n')
+            f.write('m_' + c + ' * * r *\n\n')
+            f.write('m_' + c + '1 _ '+ c +' l move \n')
+            f.write('m_' + c + '1 * * r *\n')
     static = ''
     with open ('move_circuit_static.txt', 'r') as f:
         for line in f.readlines():
@@ -32,14 +32,14 @@ def create_move_circuit(alphabet):
 def create_copy_circuit(alphabet):
     with open('copy_circuit_dynamic.txt', 'w') as f:
         for c in alphabet:
-            f.write('copy1 ' + c + ' @ l w' + c + '\n')
+            f.write('copy1 ' + c + ' @ l w_' + c + '\n')
         f.write('copy1 % _ r copy2\n')
         for c in alphabet:
-            f.write('\nw' + c + ' _ ' + c + ' r w' + c + '1\n\n')
-            f.write('w' + c + '1 $ * r w' + c + '2\n')
-            f.write('w' + c + '1 * * r *\n\n')
-            f.write('w' + c + '2 _ '+ c +' l copy \n')
-            f.write('w' + c + '2 * * r *\n')
+            f.write('\nw_' + c + ' _ ' + c + ' r w_' + c + '1\n\n')
+            f.write('w_' + c + '1 $ * r w_' + c + '2\n')
+            f.write('w_' + c + '1 * * r *\n\n')
+            f.write('w_' + c + '2 _ '+ c +' l copy \n')
+            f.write('w_' + c + '2 * * r *\n')
     static = ''
     with open ('copy_circuit_static.txt', 'r') as f:
         for line in f.readlines():
@@ -58,12 +58,12 @@ def create_copy_circuit(alphabet):
 def create_writeLeft_circuit(alphabet):
     with open('writeLeft_circuit_dynamic.txt', 'w') as f:
         for c in alphabet:
-            f.write('writeLeft2 ' + c + ' * l wl' + c + '\n')
+            f.write('writeLeft2 ' + c + ' * l wl_' + c + '\n')
         for c in alphabet:
-            f.write('\nwl' + c + ' # * l wl' + c + '1\n')
-            f.write('wl' + c + ' * * l *\n\n')
-            f.write('wl' + c + '1 _ '+ c +' r writeLeft3 \n')
-            f.write('wl' + c + '1 * * l *\n')
+            f.write('\nwl_' + c + ' # * l wl_' + c + '1\n')
+            f.write('wl_' + c + ' * * l *\n\n')
+            f.write('wl_' + c + '1 _ '+ c +' r writeLeft3 \n')
+            f.write('wl_' + c + '1 * * l *\n')
     static = ''
     with open ('writeLeft_circuit_static.txt', 'r') as f:
         for line in f.readlines():
@@ -82,13 +82,13 @@ def create_writeLeft_circuit(alphabet):
 def create_moveToFront_circuit(alphabet):
     with open('moveToFront_circuit_dynamic.txt', 'w') as f:
         for c in alphabet:
-            f.write('moveToFront1 ' + c + ' @ r mf' + c + '\n')
+            f.write('moveToFront1 ' + c + ' @ r mf_' + c + '\n')
         f.write('moveToFront1 % _ r moveToFront2\n')
         for c in alphabet:
-            f.write('\nmf' + c + ' # * l mf' + c + '1\n')
-            f.write('mf' + c + ' * * r *\n\n')
-            f.write('mf' + c + '1 _ '+ c +' l moveToFront \n')
-            f.write('mf' + c + '1 * * l *\n')
+            f.write('\nmf_' + c + ' # * l mf_' + c + '1\n')
+            f.write('mf_' + c + ' * * r *\n\n')
+            f.write('mf_' + c + '1 _ '+ c +' l moveToFront \n')
+            f.write('mf_' + c + '1 * * l *\n')
     static = ''
     with open ('moveToFront_circuit_static.txt', 'r') as f:
         for line in f.readlines():
@@ -107,13 +107,13 @@ def create_moveToFront_circuit(alphabet):
 def create_find_circuit(alphabet):
     with open('find_circuit_dynamic.txt', 'w') as f:
         for c in alphabet:
-            f.write('find ' + c + ' _ r f' + c + '\n')
+            f.write('find ' + c + ' _ r f_' + c + '\n')
         for c in alphabet:
-            f.write('\nf' + c + ' ' + c + ' * l moveWord\n')
-            f.write('f' + c + ' * * r f' + c + '1\n')
-            f.write('f' + c + ' _ * r *\n\n')
-            f.write('f' + c + '1 _ * r f' + c + '\n')
-            f.write('f' + c + '1 * * r *\n')
+            f.write('\nf_' + c + ' ' + c + ' * l moveWord\n')
+            f.write('f_' + c + ' * * r f_' + c + '1\n')
+            f.write('f_' + c + ' _ * r *\n\n')
+            f.write('f_' + c + '1 _ * r f_' + c + '\n')
+            f.write('f_' + c + '1 * * r *\n')
     static = ''
     with open ('find_circuit_static.txt', 'r') as f:
         for line in f.readlines():
